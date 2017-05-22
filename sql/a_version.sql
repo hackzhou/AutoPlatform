0eb9414e591e05122ad41257bbfbdd8fb0701bd9
Administrator <Administrator@20170419-141915>
2017-05-22 10:43:03 +0800

    update sql

--- sql/a_version.sql
+++ sql/a_version.sql
@@ -10,19 +10,20 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-05-19 19:03:05
+Date: 2017-05-22 10:42:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for a_header
+-- Table structure for a_version
 -- ----------------------------
-DROP TABLE IF EXISTS `a_header`;
-CREATE TABLE `a_header` (
+DROP TABLE IF EXISTS `a_version`;
+CREATE TABLE `a_version` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) COLLATE utf8_bin NOT NULL,
-  `value` varchar(255) COLLATE utf8_bin NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `version` varchar(255) COLLATE utf8_bin NOT NULL,
+  `channel` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
