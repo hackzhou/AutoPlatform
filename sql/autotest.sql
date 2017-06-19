7215d6823f0fbb0cce4693c0737fdf176fad5efe
Administrator <Administrator@20170419-141915>
2017-06-19 09:37:19 +0800

    update mysql

---
+++ sql/autotest.sql
@@ -0,0 +1,160 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : autotest
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-06-19 09:30:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for a_account
+-- ----------------------------
+DROP TABLE IF EXISTS `a_account`;
+CREATE TABLE `a_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `loginname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_case
+-- ----------------------------
+DROP TABLE IF EXISTS `a_case`;
+CREATE TABLE `a_case` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `interface_id` int(11) NOT NULL,
+  `version_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `run` int(1) NOT NULL,
+  `link` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `body` text COLLATE utf8_bin,
+  `result` text COLLATE utf8_bin,
+  `strategy` varchar(2048) COLLATE utf8_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_interface
+-- ----------------------------
+DROP TABLE IF EXISTS `a_interface`;
+CREATE TABLE `a_interface` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `url` varchar(2083) COLLATE utf8_bin NOT NULL,
+  `description` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_project
+-- ----------------------------
+DROP TABLE IF EXISTS `a_project`;
+CREATE TABLE `a_project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `path` varchar(255) COLLATE utf8_bin NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_result
+-- ----------------------------
+DROP TABLE IF EXISTS `a_result`;
+CREATE TABLE `a_result` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `version_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `runby` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `success` int(9) DEFAULT NULL,
+  `fail` int(9) DEFAULT NULL,
+  `total` int(9) DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `msg` varchar(2048) COLLATE utf8_bin DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_result_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `a_result_detail`;
+CREATE TABLE `a_result_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `result_id` int(11) NOT NULL,
+  `case_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `url` varchar(2083) COLLATE utf8_bin NOT NULL,
+  `description` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `strategy` varchar(2048) COLLATE utf8_bin DEFAULT NULL,
+  `body` text COLLATE utf8_bin,
+  `resulta` text COLLATE utf8_bin,
+  `resultb` text COLLATE utf8_bin,
+  `status` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `version` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `channel` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `account` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_user
+-- ----------------------------
+DROP TABLE IF EXISTS `a_user`;
+CREATE TABLE `a_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_unique` (`username`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for a_version
+-- ----------------------------
+DROP TABLE IF EXISTS `a_version`;
+CREATE TABLE `a_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` varchar(255) COLLATE utf8_bin NOT NULL,
+  `channel` varchar(2048) COLLATE utf8_bin NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `memo` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
